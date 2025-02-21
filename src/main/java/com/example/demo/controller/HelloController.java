a98f18dad90a3762a4b06f177732b3758d77614a
Anandsoni992 <anandsoni9165@gmail.com>
2025-02-21 18:45:49 +0530

    Anand Add . Rest call to show Hello mark

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
